e582dd07ab4f7cf5051e96af24f2224c7ed2056d
camden.mckinley <camden.mckinley@mtbethelchristian.org>
2020-09-30 22:45:08 -0400

    Updating Jetsam_Camden code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot_Jetsam_Camden/DriveTrain/DualMotorDrive.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.Bot_Jetsam_Camden.DriveTrain;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class DualMotorDrive {
+
+    //declaring variable
+    public DcMotor frontLeftMotor;
+    public DcMotor frontRightMotor;
+
+    //defining method
+
+    //stop command
+    public void stop () {
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+
+    }
+    //forward command
+    public void driveForward (double power){
+        frontLeftMotor.setPower(power);
+        frontRightMotor.setPower(power);
+    }
+    //left command
+    public void turnLeft (double power){
+        frontLeftMotor.setPower(-power);
+        frontRightMotor.setPower(power);
+
+    }
+    //right command
+    public void turnRight(double power){
+        frontLeftMotor.setPower(-power);
+        frontRightMotor.setPower(0);
+    }
+    //reverse command
+    public void driveReverse(double power){
+        frontRightMotor.setPower(-power);
+        frontRightMotor.setPower(-power);
+    }
+}
