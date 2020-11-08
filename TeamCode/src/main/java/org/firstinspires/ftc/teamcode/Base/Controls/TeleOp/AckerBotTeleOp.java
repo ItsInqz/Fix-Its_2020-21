2ee3f49e4c899d1d2114f18c8e06d025d1834c37
AckerTech <jamie.acker@mtbethelchristian.org>
2020-11-07 22:37:38 -0500

    Updated with LED Light Christmas Controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/Controls/TeleOp/AckerBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/Controls/TeleOp/AckerBotTeleOp.java
@@ -2,9 +2,9 @@ package org.firstinspires.ftc.teamcode.Base.Controls.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-
 import org.firstinspires.ftc.teamcode.Base.Robot.AckerBot;
 
 
@@ -13,9 +13,6 @@ import org.firstinspires.ftc.teamcode.Base.Robot.AckerBot;
 
 public class AckerBotTeleOp extends OpMode {
 
-    // Object Construction
-    public ElapsedTime TeleOpTime = new ElapsedTime();
-    public AckerBot Bot = new AckerBot();
 
     // Variables & Constants specific to TeleLabBot
     double leftStickYVal;
@@ -29,6 +26,12 @@ public class AckerBotTeleOp extends OpMode {
 
     double powerThreshold = 0;
     double speedMultiply = 1;
+    boolean reverseModeToggle = true;
+
+    // Object Construction
+    public ElapsedTime TeleOpTime = new ElapsedTime();
+    public AckerBot Bot = new AckerBot();
+
 
 
     // Runs ONCE when driver presses INIT
@@ -65,7 +68,7 @@ public class AckerBotTeleOp extends OpMode {
     public void loop() {
 
         drive();
-        slowDrive();
+        driveMode();
         Bot.christmasPattern();
         //telemetryOutput();
 
@@ -95,64 +98,135 @@ public class AckerBotTeleOp extends OpMode {
 
     public void drive () {
 
-        leftStickYVal = -gamepad1.left_stick_y;
-        leftStickYVal = Range.clip(leftStickYVal, -1, 1);
-        leftStickXVal = gamepad1.left_stick_x;
-        leftStickXVal = Range.clip(leftStickXVal, -1, 1);
-        rightStickXVal = gamepad1.right_stick_x;
-        rightStickXVal = Range.clip(rightStickXVal, -1, 1);
+        if (reverseModeToggle) {
 
-        frontLeftSpeed = leftStickYVal + leftStickXVal + rightStickXVal;
-        frontLeftSpeed = Range.clip(frontLeftSpeed, -1, 1);
+            leftStickYVal = -gamepad1.left_stick_y;
+            leftStickYVal = Range.clip(leftStickYVal, -1, 1);
+            leftStickXVal = gamepad1.left_stick_x;
+            leftStickXVal = Range.clip(leftStickXVal, -1, 1);
+            rightStickXVal = -gamepad1.right_stick_x;
+            rightStickXVal = Range.clip(rightStickXVal, -1, 1);
 
-        frontRightSpeed = leftStickYVal - leftStickXVal - rightStickXVal;
-        frontRightSpeed = Range.clip(frontRightSpeed, -1, 1);
+            Bot.frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            Bot.rearLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            Bot.frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            Bot.rearRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        rearLeftSpeed = leftStickYVal - leftStickXVal + rightStickXVal;
-        rearLeftSpeed = Range.clip(rearLeftSpeed, -1, 1);
+            frontLeftSpeed = leftStickYVal + leftStickXVal + rightStickXVal;
+            frontLeftSpeed = Range.clip(frontLeftSpeed, -1, 1);
 
-        rearRightSpeed = leftStickYVal + leftStickXVal - rightStickXVal;
-        rearRightSpeed = Range.clip(rearRightSpeed, -1, 1);
+            frontRightSpeed = leftStickYVal - leftStickXVal - rightStickXVal;
+            frontRightSpeed = Range.clip(frontRightSpeed, -1, 1);
 
+            rearLeftSpeed = leftStickYVal - leftStickXVal + rightStickXVal;
+            rearLeftSpeed = Range.clip(rearLeftSpeed, -1, 1);
 
-        if (frontLeftSpeed <= powerThreshold && frontLeftSpeed >= -powerThreshold) {
-            frontLeftSpeed = 0;
-            Bot.frontLeftMotor.setPower(frontLeftSpeed * speedMultiply);
-        } else {
-            Bot.frontLeftMotor.setPower(frontLeftSpeed * speedMultiply);
+            rearRightSpeed = leftStickYVal + leftStickXVal - rightStickXVal;
+            rearRightSpeed = Range.clip(rearRightSpeed, -1, 1);
+
+            if (frontLeftSpeed <= powerThreshold && frontLeftSpeed >= -powerThreshold) {
+                frontLeftSpeed = 0;
+                Bot.frontLeftMotor.setPower(frontLeftSpeed);
+            } else {
+                Bot.frontLeftMotor.setPower(frontLeftSpeed * speedMultiply);
+            }
+
+            if (frontRightSpeed <= powerThreshold && frontRightSpeed >= -powerThreshold){
+                frontRightSpeed = 0;
+                Bot.frontRightMotor.setPower(frontRightSpeed);
+            } else {
+                Bot.frontRightMotor.setPower(frontRightSpeed * speedMultiply);
+            }
+
+            if (rearLeftSpeed <= powerThreshold && rearLeftSpeed >= -powerThreshold) {
+                rearLeftSpeed = 0;
+                Bot.rearLeftMotor.setPower(rearLeftSpeed);
+            } else {
+                Bot.rearLeftMotor.setPower(rearLeftSpeed * speedMultiply);
+            }
+
+            if (rearRightSpeed <= powerThreshold && rearRightSpeed >= -powerThreshold){
+                rearRightSpeed = 0;
+                Bot.rearRightMotor.setPower(rearRightSpeed);
+            } else {
+                Bot.rearRightMotor.setPower(rearRightSpeed * speedMultiply);
+            }
         }
 
-        if (frontRightSpeed <= powerThreshold && frontRightSpeed >= -powerThreshold){
-            frontRightSpeed = 0;
-            Bot.frontRightMotor.setPower(frontRightSpeed * speedMultiply);
-        } else {
-            Bot.frontRightMotor.setPower(frontRightSpeed * speedMultiply);
-        }
+        else {
 
-        if (rearLeftSpeed <= powerThreshold && rearLeftSpeed >= -powerThreshold) {
-            rearLeftSpeed = 0;
-            Bot.rearLeftMotor.setPower(rearLeftSpeed * speedMultiply);
-        } else {
-            Bot.rearLeftMotor.setPower(rearLeftSpeed * speedMultiply);
-        }
+            leftStickYVal = -gamepad1.left_stick_y;
+            leftStickYVal = Range.clip(leftStickYVal, -1, 1);
+            leftStickXVal = gamepad1.left_stick_x;
+            leftStickXVal = Range.clip(leftStickXVal, -1, 1);
+            rightStickXVal = gamepad1.right_stick_x;
+            rightStickXVal = Range.clip(rightStickXVal, -1, 1);
+            Bot.frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            Bot.rearLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            Bot.frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            Bot.rearRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        if (rearRightSpeed <= powerThreshold && rearRightSpeed >= -powerThreshold){
-            rearRightSpeed = 0;
-            Bot.rearRightMotor.setPower(rearRightSpeed * speedMultiply);
-        } else {
-            Bot.rearRightMotor.setPower(rearRightSpeed * speedMultiply);
+            frontLeftSpeed = leftStickYVal + leftStickXVal + rightStickXVal;
+            frontLeftSpeed = Range.clip(frontLeftSpeed, -1, 1);
+
+            frontRightSpeed = leftStickYVal - leftStickXVal - rightStickXVal;
+            frontRightSpeed = Range.clip(frontRightSpeed, -1, 1);
+
+            rearLeftSpeed = leftStickYVal - leftStickXVal + rightStickXVal;
+            rearLeftSpeed = Range.clip(rearLeftSpeed, -1, 1);
+
+            rearRightSpeed = leftStickYVal + leftStickXVal - rightStickXVal;
+            rearRightSpeed = Range.clip(rearRightSpeed, -1, 1);
+
+            if (frontLeftSpeed <= powerThreshold && frontLeftSpeed >= -powerThreshold) {
+                frontLeftSpeed = 0;
+                Bot.frontLeftMotor.setPower(frontLeftSpeed);
+            } else {
+                Bot.frontLeftMotor.setPower(frontLeftSpeed * speedMultiply);
+            }
+
+            if (frontRightSpeed <= powerThreshold && frontRightSpeed >= -powerThreshold){
+                frontRightSpeed = 0;
+                Bot.frontRightMotor.setPower(frontRightSpeed);
+            } else {
+                Bot.frontRightMotor.setPower(frontRightSpeed * speedMultiply);
+            }
+
+            if (rearLeftSpeed <= powerThreshold && rearLeftSpeed >= -powerThreshold) {
+                rearLeftSpeed = 0;
+                Bot.rearLeftMotor.setPower(rearLeftSpeed);
+            } else {
+                Bot.rearLeftMotor.setPower(rearLeftSpeed * speedMultiply);
+            }
+
+            if (rearRightSpeed <= powerThreshold && rearRightSpeed >= -powerThreshold){
+                rearRightSpeed = 0;
+                Bot.rearRightMotor.setPower(rearRightSpeed);
+            } else {
+                Bot.rearRightMotor.setPower(rearRightSpeed * speedMultiply);
+            }
         }
 
     }
 
 
-    public void slowDrive() {
-        if (gamepad1.dpad_down) {
-            speedMultiply = 0.25;
+
+    public void driveMode () {
+        if (gamepad1.left_bumper) {
+            speedMultiply = 0.5;
         }
-        else if (gamepad1.dpad_up) {
+        else if (gamepad1.right_bumper) {
             speedMultiply = 1;
         }
+
+
+        if (gamepad1.dpad_right) {
+            reverseModeToggle = true;
+        }
+        if (gamepad1.dpad_left) {
+            reverseModeToggle = false;
+        }
+
     }
 
 
