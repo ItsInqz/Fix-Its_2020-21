ce5fe04428cb6b3383becb1071451f7242051fc6
camden.mckinley <camden.mckinley@mtbethelchristian.org>
2020-11-19 16:04:44 -0500

    Fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot_Jetsam_Camden/Control/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot_Jetsam_Camden/Control/Auto.java
@@ -1,9 +1,43 @@
 package org.firstinspires.ftc.teamcode.Bot_Jetsam_Camden.Control;
 
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Bot_Jetsam_Camden.Robot.Jetsam_Bot_Camden;
+
+@Autonomous (name = "AutonomousTestJetsam")
+
+public class Auto extends LinearOpMode {
+
+    Jetsam_Bot_Camden Bot = new Jetsam_Bot_Camden();
+
+    @Override
+
+    public void runOpMode() throws InterruptedException {
+
+
+        Bot.initJetsam(hardwareMap);
+
+        telemetry.addLine("Awaiting Start");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()){
 
 
 
 
 
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
