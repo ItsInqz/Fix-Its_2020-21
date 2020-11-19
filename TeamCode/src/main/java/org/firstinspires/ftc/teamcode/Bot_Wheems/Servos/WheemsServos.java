06ee03527c4b59276bf0c2abdd2dcbbbe940daaf
roman.pumpelly <roman.pumpelly@mtbethelchristian.org>
2020-11-19 16:17:20 -0500

    roman's code 8: test package- launcher teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot_Wheems/Servos/WheemsServos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot_Wheems/Servos/WheemsServos.java
@@ -20,9 +20,9 @@ public class WheemsServos extends Lights {
     public void armStandard() {
         mainServo.setPosition(.35);
         secondaryServo.setPosition(1.2);
-        linearOp.sleep(500);
-        mainServo.setPosition(.55);
-        secondaryServo.setPosition(0.9);
+        //linearOp.sleep(500);
+        //mainServo.setPosition(.55);
+        //secondaryServo.setPosition(0.9);
     }
     public void armDown () {
         mainServo.setPosition(0.2);
